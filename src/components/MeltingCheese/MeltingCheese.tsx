020aa1dee401d2ff504186d270932b4ae16519a4
peperonifront <cindyisaac183@gmail.com>
2025-06-13 06:53:09 -0400

    feat: implement react headroom behavior - not with the npm component

--- src/components/MeltingCheese/MeltingCheese.tsx
+++ src/components/MeltingCheese/MeltingCheese.tsx
@@ -4,7 +4,7 @@ import { meltingCheese } from "../../assets";
 const MeltingCheese = () => {
   const cheesePositions = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   return (
-    <div className="absolute z-50">
+    <div className="z-50">
       {cheesePositions.map((left, index) => (
         <img
           key={index}
