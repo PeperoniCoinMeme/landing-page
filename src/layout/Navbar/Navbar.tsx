38e943422ed0bbcf263fe38c4442cb87fe7b2e60
@llamos_isaac <157665903+RoldanIsaac@users.noreply.github.com>
2025-07-07 15:35:43 -0400

    fix: hero, dialog bug fix, oven img as div container

--- src/layout/Navbar/Navbar.tsx
+++ src/layout/Navbar/Navbar.tsx
@@ -19,22 +19,22 @@ import MeltingCheese from "../../components/MeltingCheese/MeltingCheese";
 import FlyoutLink from "./Flyout/Flyout";
 import Headroom from "../../components/Headroom/Headroom";
 import Social from "@/components/Social/Social";
-
-const menuItems = [
-  { label: "Home", href: "#hero", icon: FireIcon },
-  { label: "About", href: "#about", icon: FireIcon },
-  { label: "Mission & Vibes", href: "#missionvibes", icon: FireIcon },
-  { label: "Slice of Utility", href: "#sliceofutility", icon: FireIcon },
-  { label: "Slicenomics", href: "#slicenomics", icon: FireIcon },
-  { label: "RoadMap", href: "#roadmap", icon: FireIcon },
-  { label: "Join to the Pizza", href: "#join", icon: FireIcon },
-];
+import Dialog from "@mui/material/Dialog";
+import NavbarMenu from "@/components/NavbarMenu/NavbarMenu";
 
 const Navbar = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
-  const toggleMenu = () => setMenuOpen((prev) => !prev);
-
+  //   const [menuOpen, setMenuOpen] = useState(false);
+  //   const toggleMenu = () => setMenuOpen((prev) => !prev);
   const [hasScrolled, setHasScrolled] = useState(false);
+
+  const [open, setDialogOpen] = useState(false);
+  const handleClose = () => setDialogOpen(false);
+
+  const fontStyles: React.CSSProperties = {
+    fontFamily: "Riffic",
+  };
+
+  // Effects
 
   useEffect(() => {
     const handleScroll = () => {
@@ -45,13 +45,9 @@ const Navbar = () => {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
-  const fontStyles: React.CSSProperties = {
-    fontFamily: "Riffic",
-  };
-
   useEffect(() => {
-    document.body.style.overflow = menuOpen ? "hidden" : "";
-  }, [menuOpen]);
+    document.body.style.overflow = open ? "hidden" : "";
+  }, [open]);
 
   return (
     <>
@@ -98,7 +94,7 @@ const Navbar = () => {
           </div> */}
         </div>
         <button
-          onClick={toggleMenu}
+          onClick={() => setDialogOpen(true)}
           className="text-white relative p-[14px] cursor-pointer"
         >
           <Bars3Icon className="h-7 w-7 absolute top-0 left-0" />
@@ -107,75 +103,15 @@ const Navbar = () => {
       </nav>
       <MeltingCheese />
 
-      <AnimatePresence>
-        {menuOpen && (
-          <>
-            {/* Overlay blur background */}
-            <motion.div
-              className="fixed inset-0 bg-black-800 bg-opacity-90 backdrop-blur-md z-500"
-              onClick={toggleMenu}
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-            />
+      {/* Dialog */}
+      <Dialog
+        open={open}
+        onClose={() => setDialogOpen(false)}
+        disableScrollLock
+      >
+        {open && <NavbarMenu setOpen={setDialogOpen} />}
+      </Dialog>
 
-            {/* Modal panel */}
-            <motion.div
-              className="fixed z-600 inset-0 flex items-center justify-center"
-              initial={{ opacity: 0, scale: 0.9 }}
-              animate={{ opacity: 1, scale: 1 }}
-              exit={{ opacity: 0, scale: 0.9 }}
-              transition={{ duration: 0.2, ease: "easeInOut" }}
-            >
-              <div
-                className="relative bg-dark-dough p-6 rounded-xl text-white w-6/12 space-y-6"
-                onClick={(e) => e.stopPropagation()}
-              >
-                <button
-                  onClick={toggleMenu}
-                  className="absolute cursor-pointer z-9999 -right-20 top-3 md:right-3 text-[var(--color-pepperoni)] hover:text-red-400 transition"
-                >
-                  <XMarkIcon className="h-10 w-10 m-3 text-orange-100 text-shadow-lg" />
-                </button>
-
-                <h2 className="text-5xl font-bold opacity-0">🍕</h2>
-
-                <ul className="space-y-4">
-                  {menuItems.map(({ label, href, icon: Icon }, index) => (
-                    <motion.li
-                      key={label}
-                      initial={{ opacity: 0, x: -100 }}
-                      animate={{ opacity: 1, x: 0 }}
-                      transition={{
-                        duration: 0.5,
-                        delay: index * 0.03,
-                        ease: "easeOut",
-                      }}
-                    >
-                      <a
-                        href={href}
-                        onClick={toggleMenu}
-                        className="flex text-3xl font-bold text-center justify-center hover:text-yellow-300 text-white/95 transition"
-                        style={fontStyles}
-                      >
-                        {/* <Icon className="h-5 w-5 text-yellow-500" /> */}
-                        {label}
-                      </a>
-                    </motion.li>
-                  ))}
-                </ul>
-
-                {/* Flames */}
-                {/* <CryptoFlameTrail />
-                <CryptoFlameTrail /> */}
-                <div className="absolute -bottom-14 -left-18">
-                  <Social mode="footer" />
-                </div>
-              </div>
-            </motion.div>
-          </>
-        )}
-      </AnimatePresence>
       {/* </Headroom> */}
     </>
   );
